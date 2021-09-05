265a7d7bf55dd80cc77646c99d1d2d73a7f5f701
weiiii <joegane0629@gmail.com>
2021-09-05 19:38:04 +0800

    fix

--- src/main/java/io/github/thebusybiscuit/extraheads/ExtraHeads.java
+++ src/main/java/io/github/thebusybiscuit/extraheads/ExtraHeads.java
@@ -43,9 +43,9 @@ public class ExtraHeads extends JavaPlugin implements SlimefunAddon {
         // Setting up bStats
         //new Metrics(this, 5650);
 
-        if (cfg.getBoolean("options.auto-update") && getDescription().getVersion().startsWith("EFI - ")) {
-            new GitHubBuildsUpdater(this, getFile(), "SlimeTraditionalTranslation/ExtraHeads/master").start();
-        }
+        /*if (cfg.getBoolean("options.auto-update") && getDescription().getVersion().startsWith("DEV - ")) {
+        *    new GitHubBuildsUpdater(this, getFile(), "TheBusyBiscuit/ExtraHeads/master").start();
+        *}*/
 		
         itemGroup = new ItemGroup(new NamespacedKey(this, "heads"), new CustomItemStack(PlayerHead.getItemStack(PlayerSkin.fromHashCode("5f1379a82290d7abe1efaabbc70710ff2ec02dd34ade386bc00c930c461cf932")), "&7Extra Heads", "", "&a> Click to open"), 1);
         recipeType = new RecipeType(new NamespacedKey(this, "decapitation"), new CustomItemStack(Material.IRON_SWORD, "&6殺死指定怪物"));
